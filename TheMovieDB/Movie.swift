91d46bc771ab9d9ae7dbfba81bd0f3855714cd1d
Bruno Fulber Wide <bfwide07@gmail.com>
2017-06-19 18:18:01 -0300

    changed some stuff in model

--- TheMovieDB/Movie.swift
+++ TheMovieDB/Movie.swift
@@ -10,47 +10,69 @@ import UIKit
 
 class Movie {
 
+    var id : String!
     var title : String!
     var poster : UIImage?
     var backdrop : UIImage?
-    var actors : [Actor]?
-    var releaseDate : Date?
-    var categories : [String]?
-    
-    init() {
-        self.title = "Teste"
-        self.poster = #imageLiteral(resourceName: "teste")
-    }
+    var releaseDate : String?
+    var runtime : Int?
+    var rating : Int?
+    var genres : [String]?
+    var actors : [Actor]? //TODO
     
     init(with data: Dictionary<String, AnyObject>){
+        
+        //initialize non optional variables
+        self.id = (data["id"] as! Int).description
         self.title = data["original_title"] as! String
         
-        //initialize poster if request is sucessfull
-        let posterURL =
-        URL(string: "https://image.tmdb.org/t/p/w185" + (data["poster_path"] as! String) )
+        //initialize optional variables
         
-        if let posterData = try? Data(contentsOf: posterURL!) {
+        //load poster
+        if let path = (data["poster_path"] as? String) {
             
-            self.poster = UIImage(data: posterData)
-            
-        } else {
-            print("error initializing poster")
+            let posterURL = URL(string: "https://image.tmdb.org/t/p/w185" + path )
+            loadImage(at: posterURL!, response: { self.poster = $0 })
         }
         
         //load backdrop
         if let path = (data["backdrop_path"] as? String) {
-                let backdropURL =
-                URL(string: "https://image.tmdb.org/t/p/w780" + path )
             
-                if let backdropData = try? Data(contentsOf: backdropURL!) {
-                    self.backdrop = UIImage(data: backdropData)
-                } else {
-                    print("erro")
-                }
-        } else {
-            self.backdrop = self.poster
+            let backdropURL = URL(string: "https://image.tmdb.org/t/p/w780" + path )
+            loadImage(at: backdropURL!, response: { self.backdrop = $0 })
+        }
+        
+        //load release date
+        if let release = data["release_date"] as? String {
+            self.releaseDate = release
+        }
+        
+        //load runtime
+        if let runtime = data["runtime"] as? Int {
+            self.runtime = runtime
+        }
+        
+        //load rating
+        if let rating = data["vote_average"] as? Int {
+            self.rating = rating
+        }
+        
+        //load genres
+        if let genres = data["genres"] as? [Dictionary<String, Any>] {
+            self.genres = []
+            for genre in genres{
+                self.genres?.append( genre["name"] as! String )
+            }
         }
     }
     
-    
+    private func loadImage(at url: URL, response: (UIImage) -> Void) {
+        if let imageData = try? Data(contentsOf: url) {
+            
+            response(UIImage(data: imageData)!)
+            
+        } else {
+            print("error initializing image")
+        }
+    }
 }
